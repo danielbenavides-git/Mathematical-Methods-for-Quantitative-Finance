1cb8ee3ef93bd7738f8d4d7105d28be637025ddd
Daniel Benavides <d.benavidess@uniandes.edu.co>
2026-01-12 22:29:18 -0500

    Finished module 2.2

--- Module 2 - Introduction to Discrete-Time Stochastic Processes/2.1_lecture_exercises.R
+++ Module 2 - Introduction to Discrete-Time Stochastic Processes/2.1_lecture_exercises.R
@@ -1,2 +1,31 @@
-# Exercise 1.3: Expectations and Moments
+# Exercise 1.3: Generalized Random Walk
 
+T = c(2, 4, 6, 8, 10)
+mu = 2
+sigma = 3
+
+for (t in T) {
+  E_XT = t * mu
+  Var_XT = t * sigma^2
+  r_T = E_XT / sqrt(Var_XT)
+  
+  print(paste(
+    "For T =", t,
+    ": E[X_T] =", E_XT,
+    ", Var[X_T] =", Var_XT,
+    ", r_T =", r_T
+  ))
+}
+
+# Exercise 2.2: Solving AR(1) Model
+
+mu = 0.1
+lambda = 0.2
+sigma = 0.3
+
+k = 1
+
+gamma_0 = sigma^2 / (1 - lambda^2)
+gamma_1 = (-lambda)^k * gamma_0
+
+print(paste("Gamma_1 for k =", k, "is", gamma_1))
