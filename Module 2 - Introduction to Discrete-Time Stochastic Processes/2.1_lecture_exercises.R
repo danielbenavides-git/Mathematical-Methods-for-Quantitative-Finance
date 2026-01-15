cd08e6f0f15a3a395ff9e540044ee2a344fd4b4c
Daniel Benavides <d.benavidess@uniandes.edu.co>
2026-01-14 20:54:51 -0500

    Monte Carlo Sim

--- Module 2 - Introduction to Discrete-Time Stochastic Processes/2.1_lecture_exercises.R
+++ Module 2 - Introduction to Discrete-Time Stochastic Processes/2.1_lecture_exercises.R
@@ -29,3 +29,27 @@ gamma_0 = sigma^2 / (1 - lambda^2)
 gamma_1 = (-lambda)^k * gamma_0
 
 print(paste("Gamma_1 for k =", k, "is", gamma_1))
+
+# Exercise 3.2: Monte Carlo Simulation
+
+p <- 0.5;q <- 1-p; # Set probability of "success" and "failure"
+Nt <- 20; # Number of time steps
+Np <- 1; # Number of sample paths
+z <- matrix(runif(Nt*Np),nrow=Nt) # Generate a set of uniform random draws
+x <- sign(p-z); # Transform to binomial random variable +/- 1
+s <- matrix(0,Nt+1,Np) # Initial value for random walk.
+# Note that R indices start with 1,not zero.
+for (t in 1:Nt) {
+  s[t+1,] <- s[t,]+x[t,] # New location equals previous plus a new random step
+}
+plot(s,type="b") # Plot resulting path
+
+Nt <- 252; # Number of trading days in a year
+Np <- 1e4; # Reasonably large number of simulations
+z <- matrix(runif(Nt*Np),nrow=Nt) # Generate a set of uniform random draws
+x <- sign(p-z); # Transform to binomial random variable +/- 1
+s <- matrix(0,Nt+1,Np) # Initialize and reserve space for random paths.
+for (t in 1:Nt) {
+  s[t+1,] <- s[t,]+x[t,] # New location equals previous plus a new random step
+}
+matplot(s[,1:3],type="b") # Plot a few of the resulting paths
